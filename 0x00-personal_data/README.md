843176b16ae5eba4b124d0a7888042a7e64c7f22
Ramyrashad127 <ramy41599@gmail.com>
2024-09-16 23:52:18 +0300

    new

---
+++ 0x00-personal_data/README.md
@@ -0,0 +1,2 @@
+authenticate to a database using environment variables
+How to encrypt a password and check the validity of an input password
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+"""import modules"""
+import re
+from typing import List
+
+
+def filter_datum(fields :List[str], redaction :str, message :str, separator :str) -> str:
+    """filter_datum function"""
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}', f'{field}={redaction}{separator}', message)
+    return message
