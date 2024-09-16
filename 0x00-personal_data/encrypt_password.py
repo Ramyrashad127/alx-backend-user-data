3605bc93ddf09729104873570d0317c668e89507
Ramyrashad127 <ramy41599@gmail.com>
2024-09-17 00:35:59 +0300

    new

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,8 @@ def hash_password(password: str) -> bytes:
     """ Returns a hashed password """
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Returns a boolean """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
