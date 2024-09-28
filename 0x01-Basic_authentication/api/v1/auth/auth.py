a43c507232801203dd44678d9fd5137a1e66f021
Ramyrashad127 <ramy41599@gmail.com>
2024-09-28 23:50:32 +0300

    new

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,8 +21,6 @@ class Auth:
         """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        if path[-1] != '/':
-            path += '/'
         if path in excluded_paths:
             return False
         return True
