d62fb234ec366ea51981443433b0cbbd3b433d46
Ramyrashad127 <ramy41599@gmail.com>
2024-09-30 00:18:37 +0300

    new

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -21,3 +21,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+                                            self,
+                                            base64_authorization_header: str
+                                            ) -> str:
+        """ returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            return base64_authorization_header.encode('utf-8').decode('base64')
+        except Exception:
+            return None
