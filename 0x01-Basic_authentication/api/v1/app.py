6197b7597f7a3bc71e45070dad53cdfa4e01366f
Ramyrashad127 <ramy41599@gmail.com>
2024-09-29 23:49:29 +0300

    new

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
@@ -15,6 +14,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 if os.getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
